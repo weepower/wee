2e3b2ba3f3f9a94adf8f6ccb7c8cdd21953db9e1
Michael Leigeber <michael@caddis.co>
2014-08-29 10:56:20 -0500

    Resolve scope issue with placeholder polyfill

--- public/assets/js/polyfill/wee.placeholder.js
+++ public/assets/js/polyfill/wee.placeholder.js
@@ -9,25 +9,26 @@ this.attachEvent('onload', function() {
 			i = 0;
 
 		for (; i < len; i++) {
-			var el = arr[i],
-				val = el.getAttribute('placeholder');
+			(function() {
+				var el = arr[i],
+					val = el.getAttribute('placeholder');
 
-			if (el.value == '') {
-				el.value = val;
-			}
-
-			el.onblur = function() {
-				if (this.value.replace(/^\s+|\s+$/g, '') == '') {
-					this.value = val;
+				if (el.value == '') {
+					el.value = val;
 				}
-			};
 
-			el.onfocus = function() {
-				if (this.value == val) {
-					console.log('match');
-					this.value = '';
-				}
-			};
+				el.onblur = function() {
+					if (this.value.replace(/^\s+|\s+$/g, '') == '') {
+						this.value = val;
+					}
+				};
+
+				el.onfocus = function() {
+					if (this.value == val) {
+						this.value = '';
+					}
+				};
+			})();
 		}
 
 		arr = d.querySelectorAll('form');
@@ -36,21 +37,23 @@ this.attachEvent('onload', function() {
 
 		// Clear default placeholder values on form submit
 		for (; i < len; i++) {
-			var el = arr[i];
+			(function() {
+				var el = arr[i];
 
-			el.onsubmit = function() {
-				var inputs = el.querySelectorAll('input[placeholder], textarea[placeholder]'),
-					xlen = inputs.length,
-					x = 0;
+				el.onsubmit = function() {
+					var inputs = el.querySelectorAll('input[placeholder], textarea[placeholder]'),
+						xlen = inputs.length,
+						x = 0;
 
-				for (; x < xlen; x++) {
-					var xel = inputs[x];
+					for (; x < xlen; x++) {
+						var xel = inputs[x];
 
-					if (xel.value == xel.getAttribute('placeholder')) {
-						xel.value = '';
+						if (xel.value == xel.getAttribute('placeholder')) {
+							xel.value = '';
+						}
 					}
 				}
-			}
+			})();
 		}
 	})(document);
 });
