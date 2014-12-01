291ca8b9c1912f67f4be2b972bfea930cee00bab
Michael Leigeber <michael@caddis.co>
2014-12-01 12:21:03 -0600

    Add standalone validation task to validate all script assets

--- public/assets/wee/build/tasks/validate.js
+++ public/assets/wee/build/tasks/validate.js
@@ -1,5 +1,16 @@
+/* global config */
+
 module.exports = function(grunt) {
-	grunt.registerTask('validate', function() {
-		// Validate all project files
+	grunt.registerTask('runValidation', function() {
+		var Wee = require('../core.js'),
+			scriptPath = config.assetPath + '/js',
+			scripts = Wee.getFiles(scriptPath, 'js');
+
+		// Validate scripts
+		scripts.forEach(function(filepath) {
+			if (filepath.indexOf('.min.js') == -1) {
+				Wee.validate(config, grunt, filepath);
+			}
+		});
 	});
 };
