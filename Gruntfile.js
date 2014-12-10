2c3206f26965656ed90360bcfc17d4f56c4809f3
Michael Leigeber <michael@caddis.co>
2014-12-10 10:18:56 -0600

    Simplify Gruntfile, moving all dynamic logic to wee/build

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,87 +8,17 @@
 	'use strict';
 
 	module.exports = function(grunt) {
-		var configFile = './' + (grunt.option('config') || 'project.json');
+		global.rootPath = __dirname;
+		global.configPath = './' + (grunt.option('config') || 'project.json');
 
-		global.nodePath = __dirname + '/node_modules';
-		global.project = JSON.parse(grunt.file.read(configFile));
+		global.project = grunt.file.readJSON(global.configPath);
 
 		var assetPath = (project.paths.root !== '') ?
 				project.paths.root + '/' + project.paths.assets :
-				project.assets;
+				project.assets,
+			buildPath = assetPath + '/wee/build/';
 
-		// ------------------------------------
-			// Load Modules
-		// ------------------------------------
-
-		if (project.script.validate.jscs) {
-			global.jscs = require('jscs');
-		}
-
-		// ------------------------------------
-			// Load Tasks
-		// ------------------------------------
-
-		require('./' + assetPath + '/wee/build/config.js')(grunt);
-		grunt.loadTasks(assetPath + '/wee/build/tasks');
-
-		// -------------------------------------
-			// Load Plugins
-		// -------------------------------------
-
-		grunt.loadNpmTasks('grunt-browser-sync');
-		grunt.loadNpmTasks('grunt-contrib-concat');
-		grunt.loadNpmTasks('grunt-contrib-imagemin');
-		grunt.loadNpmTasks('grunt-contrib-less');
-		grunt.loadNpmTasks('grunt-contrib-uglify');
-		grunt.loadNpmTasks('grunt-contrib-watch');
-		grunt.loadNpmTasks('grunt-newer');
-		grunt.loadNpmTasks('grunt-notify');
-
-		// -----------------------------------
-			// Grunt Tasks
-		// -----------------------------------
-
-		grunt.registerTask('default', [
-			'init',
-			'cleanup',
-			'configStyle',
-			'configScript',
-			'configModules',
-			'bindConfig',
-			'buildStyle',
-			'buildLegacy',
-			'initGuide',
-			'uglify:core',
-			'uglify:lib',
-			'imagemin'
-		]);
-
-		// Build + Watch
-		grunt.registerTask('local', [
-			'default',
-			'reload',
-			'proxy',
-			'watch'
-		]);
-
-		// Build + Server + Open + Watch
-		grunt.registerTask('static', [
-			'default',
-			'reload',
-			'server',
-			'watch'
-		]);
-
-		// Update
-		grunt.registerTask('update', [
-			'checkUpdates'
-		]);
-
-		// Validate
-		grunt.registerTask('validate', [
-			'init',
-			'runValidation'
-		]);
+		require('./' + buildPath + 'config.js')(grunt);
+		grunt.loadTasks(buildPath + 'tasks');
 	};
 })();
