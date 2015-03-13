404c14914c050facde7968d142249cfd7077c9fe
Michael Leigeber <michael@caddis.co>
2015-03-13 13:52:34 -0500

    Create 2.1.1 version Update browser-sync dependency to 2.2.4

--- public/assets/wee/build/config.js
+++ public/assets/wee/build/config.js
@@ -136,7 +136,7 @@ module.exports = function(grunt) {
 	global.reloadPaths = [];
 	global.legacyBuild = [];
 	global.legacyConvert = [];
-	global.version = '2.1.0';
+	global.version = '2.1.1';
 
 	grunt.initConfig({
 		less: {
