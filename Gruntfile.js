f9dd2958388d05f0871a5940b6c5caa58c42bba4
Michael Leigeber <michael@caddis.co>
2015-08-09 10:11:18 -0500

    Simplify Grunt and CLI scripts and pass in CWD

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,8 @@
 // Licensed under Apache 2 (http://www.apache.org/licenses/LICENSE-2.0)
 // DO NOT MODIFY
 
-/* global __dirname, global */
+/* global __dirname */
 
-(function() {
-	'use strict';
-
-	module.exports = function(grunt) {
-		var buildPath = './node_modules/wee-core/build/';
-
-		global.rootPath = __dirname;
-		global.configPath = './' + (grunt.option('config') || 'wee.json');
-
-		require(buildPath + 'config.js')(grunt);
-		grunt.loadTasks(buildPath + 'tasks');
-	};
-})();
+module.exports = function(grunt) {
+	require(__dirname + '/node_modules/wee-core/build/config.js')(grunt, __dirname);
+};
--- wee.js
+++ wee.js
@@ -2,4 +2,6 @@
 // Licensed under Apache 2 (http://www.apache.org/licenses/LICENSE-2.0)
 // DO NOT MODIFY
 
-require('./node_modules/wee-core/cli.js');
+/* global __dirname */
+
+require(__dirname + '/node_modules/wee-core/cli.js')(__dirname);
