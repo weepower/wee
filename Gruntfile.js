0c814373d631deb1bba6d5106cd0bab0960cf04a
Michael Leigeber <michael@leigeber.com>
2015-10-15 19:52:15 -0500

    Move Gruntfile and dependencies entirely within wee-core from wee Update version to beta 3

--- Gruntfile.js
+++
@@ -1,9 +0,0 @@
-// Wee (weepower.com)
-// Licensed under Apache 2 (http://www.apache.org/licenses/LICENSE-2.0)
-// DO NOT MODIFY
-
-/* global __dirname */
-
-module.exports = function(grunt) {
-	require(__dirname + '/node_modules/wee-core/build/config.js')(grunt, __dirname);
-};
