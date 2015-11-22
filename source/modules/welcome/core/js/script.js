c52c2862ae1eac033d5e230ac90da75b9515a231
Michael Leigeber <michael@leigeber.com>
2015-11-22 15:20:06 -0600

    Split out Welcome todo controller into a separate file

--- source/modules/welcome/core/js/script.js
+++ source/modules/welcome/core/js/script.js
@@ -1,39 +1,3 @@
-Wee.fn.make('todo', {
-	init: function() {
-		var app = Wee.app.make('todo', {
-			view: 'welcome.todo',
-			target: 'ref:todo',
-			model: {
-				todo: [
-					{
-						label: 'Download and run Wee',
-						done: true
-					},
-					{
-						label: 'Explore the guide and welcome modules'
-					},
-					{
-						label: 'Reset your project to a blank slate'
-					},
-					{
-						label: 'Customize wee.json'
-					},
-					{
-						label: 'Build something incredible'
-					}
-				]
-			}
-		});
-
-		$('ref:toggle').on('click', function() {
-			var id = $(this).data('id'),
-				done = app.$get('todo.' + id + '.done');
-
-			app.$set('todo.' + id + '.done', done ? false : true);
-		});
-	}
-});
-
 Wee.routes.map({
 	'$root': 'todo'
 }, true);
