8f1534e9a799ac70f51254ab249f5bfc926ecb10
Michael Leigeber <michael@leigeber.com>
2015-12-03 12:58:17 -0600

    Add example route and common controller Update version to 3.0.1

---
+++ source/js/build/common.js
@@ -0,0 +1,14 @@
+// Controller documentation available at https://www.weepower.com/script/core#fnmake
+
+Wee.fn.make('common', {
+	_construct: function() {
+		// ...
+	},
+	init: function() {
+		this.$private.method();
+	}
+}, {
+	method: function() {
+		// ...
+	}
+});
--- source/js/custom/script.js
+++ source/js/custom/script.js
@@ -1,1 +1,8 @@
 // Add custom JavaScript here
+// Route documentation available at https://www.weepower.com/script/routes
+
+Wee.routes.map({
+	'$root': 'common'
+});
+
+Wee.ready('routes:run');
