b630033600806fff92158092217bd8bc3ed10e7e
Michael Leigeber <michael@leigeber.com>
2016-02-06 21:53:23 -0600

    Update welcome model app to use root data storage

--- source/modules/welcome/core/js/todo.js
+++ source/modules/welcome/core/js/todo.js
@@ -3,33 +3,31 @@ Wee.fn.make('todo', {
 		var app = Wee.app.make('todo', {
 			view: 'welcome.todo',
 			target: 'ref:todo',
-			model: {
-				todo: [
-					{
-						label: 'Download and run Wee',
-						done: true
-					},
-					{
-						label: 'Explore the guide and welcome modules'
-					},
-					{
-						label: 'Reset your project to a blank slate'
-					},
-					{
-						label: 'Customize wee.json'
-					},
-					{
-						label: 'Build something incredible'
-					}
-				]
-			}
+			model: [
+				{
+					label: 'Download and run Wee',
+					done: true
+				},
+				{
+					label: 'Explore the guide and welcome modules'
+				},
+				{
+					label: 'Reset your project to a blank slate'
+				},
+				{
+					label: 'Customize wee.json'
+				},
+				{
+					label: 'Build something incredible'
+				}
+			]
 		});
 
 		$('ref:toggle').on('click', function() {
 			var id = $(this).data('id'),
-				done = app.$get('todo.' + id + '.done');
+				val = app.$get(id + '.done') ? false : true;
 
-			app.$set('todo.' + id + '.done', done ? false : true);
+			app.$set(id + '.done', val);
 		});
 	}
 });
