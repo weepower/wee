311e13c8cb52277f21eb0603574900d91b2b6b78
Nathan Hood <nathanhood@me.com>
2017-04-05 13:33:58 -0500

    Update paths to declarations and rules in mixins file

--- source/styles/mixins.js
+++ source/styles/mixins.js
@@ -1,5 +1,5 @@
-// const Decl = require('postcss-js-mixins/lib/Declaration');
-// const Rule = require('postcss-js-mixins/lib/Rule');
+// const Decl = require('postcss-js-mixins/lib/declaration');
+// const Rule = require('postcss-js-mixins/lib/rule');
 // const { isObject, isEmpty, isPercentage, isColor, prefix, isNumber, hexToRgba, calcOpacity, isString, unit } = require('postcss-js-mixins/lib/helpers');
 
 module.exports = (vars = {}) => {
