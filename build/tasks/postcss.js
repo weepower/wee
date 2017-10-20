5f4169f4db648415f8bc770b6a69564fc1fa0666
Peter Bedor <peterbedor@gmail.com>
2017-10-20 11:00:34 -0500

    Rework sass/postcss build process

--- build/tasks/postcss.js
+++ build/tasks/postcss.js
@@ -1,13 +1,11 @@
-const postcss = require('postcss');
-const glob = require('glob');
 const fs = require('fs-extra');
-const chalk = require('chalk');
+const glob = require('glob');
+const postcss = require('postcss');
 const sass = require('node-sass');
 const paths = require('../paths');
 const log = require('../utils').log;
 
-// Config
-const config = fs.readJsonSync(paths.project + '/wee.json');
+const config = readFile(paths.project + '/wee.json', true);
 const breakpoints = config.style.breakpoints;
 const features = config.style.features;
 const compile = config.style.compile;
@@ -20,13 +18,105 @@ const plugins = [
 		safe: true
 	})
 ];
-let ignore = [];
 
-// Main output file
-let data;
+let data = '';
 
-let mixins;
-let variables;
+// Add user variables
+data += addImport(`${paths.styles}/variables.scss`);
+
+// Add core variables
+data += addImport(`${paths.weeCore}/styles/variables.scss`);
+
+// Add core mixins
+data += addImport(`${paths.weeCore}/styles/mixins.scss`);
+
+// Add user mixins
+data += addImport(`${paths.styles}/mixins.scss`);
+
+// Add reset
+data += addImport(`${paths.weeCore}/styles/reset.scss`);
+
+// Add responsive helpers
+data += addImport(`${paths.temp}/responsive.scss`);
+
+// Add features
+for (let feature in features) {
+	if (feature !== 'print' && features[feature]) {
+		data += addImport(`${paths.weeCore}/styles/components/${feature}.scss`);
+	}
+}
+
+data += addImport(`${paths.weeCore}/styles/components/helpers.scss`);
+
+// Add print
+if (features.print) {
+	// data += addImport(`${paths.weeCore}/styles/print.scss`);
+	data += `@media print {
+		${readFile(`${paths.weeCore}/styles/print.scss`)}
+		${readFile(`${paths.styles}/print.scss`)}
+	}\n`;
+}
+
+// Add breakpoint files
+for (let breakpoint in breakpoints) {
+	let name = convertCamelToDash(breakpoint),
+		file = `${paths.styles}/breakpoints/${name}.scss`;
+
+	// Create file if not already generated
+	fs.ensureFileSync(file);
+
+	if (breakpoints[breakpoint]) {
+		data += `@${breakpoint} { ${readFile(file)} }\n`;
+	} else {
+		log.error(`Unregistered breakpoint: ${name}`);
+		log.message('Check breakpoint files against registered breakpoints in wee.json');
+	}
+}
+
+data += readFile(`${paths.styles}/screen.scss`);
+
+// Add component files
+glob.sync(paths.components + '/**/*.{scss,css}').forEach(file => {
+	data += readFile(file) + '\n';
+});
+
+config.style.build.forEach(pattern => {
+	data += getCSS(pattern);
+});
+
+// Process compile blocks
+let files = Object.keys(compile);
+
+for (let i = 0; i < files.length; i++) {
+	let result = '',
+		block = compile[files[i]];
+
+	// Not sure how to get around using sync methods to ensure
+	// compilation order as designated by array compile blocks
+	if (Array.isArray(block)) {
+		block.forEach(filePath => {
+			// Concatenate files together
+			result += getCSS(filePath);
+		});
+	} else {
+		result += getCSS(block);
+	}
+
+	processCSS(result, `${paths.output.styles}/${files[i]}`).then(() => {
+		log.message('Compiled: ' + files[i], 'red');
+	});
+}
+
+// console.log(data);
+
+/**
+ * Add import path
+ * @param path
+ * @returns {string}
+ */
+function addImport(path) {
+	return `@import "${path}";\n`;
+}
 
 /**
  * Calculate breakpoints with offset included
@@ -56,24 +146,6 @@ function convertCamelToDash(value) {
 }
 
 /**
- * Find and return file(s) content
- *
- * @param {string} filePath
- * @returns {string}
- */
-function getCSS(filePath) {
-	let base = filePath.split('/')[0] === '.' ? paths.project : paths.styles,
-		result = '';
-
-	glob.sync(`${base}/${filePath}`).forEach(file => {
-		ignore.push(file);
-		result += fs.readFileSync(file, 'utf-8');
-	});
-
-	return result;
-}
-
-/**
  * Process CSS into output
  *
  * @param {string} css
@@ -82,6 +154,9 @@ function getCSS(filePath) {
 function processCSS(css, destination) {
 	let result = sass.renderSync({
 		data: css,
+		includePaths: [
+			paths.nodeModules
+		],
 		outputStyle: 'compressed'
 	});
 
@@ -89,6 +164,8 @@ function processCSS(css, destination) {
 		.process(result.css.toString())
 		.then(result => {
 			return new Promise((resolve, reject) => {
+				fs.ensureFileSync(destination);
+
 				fs.writeFile(destination, result.css, err => {
 					if (err) {
 						reject(err);
@@ -104,125 +181,34 @@ function processCSS(css, destination) {
 }
 
 /**
- * Start of process
+ * Find and return file(s) content
+ *
+ * @param {string} filePath
+ * @returns {string}
  */
+function getCSS(filePath) {
+	let base = filePath.split('/')[0] === '.' ? paths.project : paths.styles,
+		result = '';
 
-log.heading('Compiling CSS', 'red');
-
-// Make sure output directories exist
-fs.mkdirsSync(paths.output.styles);
-
-// Process compile blocks
-let files = Object.keys(compile);
-
-for (let i = 0; i < files.length; i++) {
-	let result = '',
-		block = compile[files[i]];
-
-	// Not sure how to get around using sync methods to ensure
-	// compilation order as designated by array compile blocks
-	if (Array.isArray(block)) {
-		block.forEach(filePath => {
-			// Concatenate files together
-			result += getCSS(filePath);
-		});
-	} else {
-		result += getCSS(block);
-	}
-
-	processCSS(result, `${paths.output.styles}/${files[i]}`).then(() => {
-		log.message('Compiled: ' + files[i], 'red');
+	glob.sync(`${base}/${filePath}`).forEach(file => {
+		result += readFile(file);
 	});
+
+	return result;
 }
 
-data = '';
+/**
+ * Read a file
+ *
+ * @param path
+ * @param json
+ * @returns {*}
+ */
+function readFile(path, json) {
+	let type = json ? 'Json' : 'File';
 
-// Add user variable overrides and mixins
-variables = paths.styles + '/variables.scss';
-data += fs.readFileSync(variables, 'utf-8') + '\n';
-ignore.push(variables);
-
-// Add reset and base styling
-data += fs.readFileSync(paths.weeCore + '/styles/variables.scss', 'utf-8') + '\n';
-
-data += fs.readFileSync(paths.weeCore + '/styles/mixins.scss', 'utf-8') + '\n';
-
-mixins = paths.styles + '/mixins.scss';
-data += fs.readFileSync(mixins, 'utf-8') + '\n';
-ignore.push(mixins);
-
-data += fs.readFileSync(paths.weeCore + '/styles/reset.scss', 'utf-8') + '\n';
-data += fs.readFileSync(__dirname + '/../temp/responsive.scss', 'utf-8') + '\n';
-
-// Add features
-if (features.buttons) {
-	data += fs.readFileSync(paths.weeCore + '/styles/components/buttons.scss', 'utf-8') + '\n';
+	return fs[`read${type}Sync`](path, json ? '' : 'utf-8');
 }
-
-if (features.code) {
-	data += fs.readFileSync(paths.weeCore + '/styles/components/code.scss', 'utf-8') + '\n';
-}
-
-if (features.forms) {
-	data += fs.readFileSync(paths.weeCore + '/styles/components/forms.scss', 'utf-8') + '\n';
-}
-
-if (features.tables) {
-	data += fs.readFileSync(paths.weeCore + '/styles/components/tables.scss', 'utf-8') + '\n';
-}
-
-if (features.print) {
-	// Add print files
-	data += `@media print {
-		${fs.readFileSync(paths.weeCore + '/styles/print.scss', 'utf-8')}
-		${getCSS('print.scss')}
-	}\n`;
-}
-
-// Add class helpers
-data += fs.readFileSync(paths.weeCore + '/styles/components/helpers.scss', 'utf-8') + '\n';
-
-const screen = paths.styles + '/screen.scss';
-
-data += fs.readFileSync(screen, 'utf-8') + '\n';
-
-ignore.push(screen);
-
-// Add breakpoint files
-for (let breakpoint in breakpoints) {
-	let name = convertCamelToDash(breakpoint),
-		file = `${paths.styles}/breakpoints/${name}.scss`;
-
-	// Create file if not already generated
-	fs.ensureFileSync(file);
-
-	ignore.push(file);
-
-	if (breakpoints[breakpoint]) {
-		data += `@${breakpoint} { ${fs.readFileSync(file, 'utf-8')} }\n`;
-	} else {
-		log.error(`Unregistered breakpoint: ${name}`);
-		log.message('Check breakpoint files against registered breakpoints in wee.json');
-	}
-}
-
-// Add component files
-glob.sync(paths.components + '/**/*.{scss,css}').forEach(file => {
-	data += fs.readFileSync(file, 'utf-8') + '\n';
-});
-
-// Add all build files
-config.style.build.forEach(pattern => {
-	data += getCSS(pattern);
-});
-
-// Concatenate all other files
-glob.sync(paths.styles + '**/*.{scss,css}', {
-	ignore
-}).forEach(file => {
-	console.log(file);
-	data += fs.readFileSync(file, 'utf-8') + '\n';
-});
 
 // Process main style file
 processCSS(data, paths.output.styles + '/style.min.css').then(() => {
