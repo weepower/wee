2280917040b7f8fd4257d303b7211c17dc816105
Peter Bedor <peter@lewiscommunications.com>
2018-01-26 00:20:22 -0600

    Update to spaces

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -1,13 +1,11 @@
 import './bootstrap';
 
-/**
- * Begin your application here
- *
- * Example:
- *
- * import $router from 'wee-routes';
- *
- * $router.map([
- * 		{ path: '/', init() { 'Hello world' } }
- * ]).run();
- */
+
+// Begin your application here
+// Example:
+
+// import $router from 'wee-routes';
+
+// $router.map([
+//     { path: '/', init() { 'Hello World' } }
+// ]).run();
