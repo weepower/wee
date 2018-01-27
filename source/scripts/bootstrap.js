092a6c5725637854255709ec51fc730bc4f8ad04
Peter Bedor <peter@lewiscommunications.com>
2018-01-27 00:11:24 -0600

    Add styles to entry point

--- source/scripts/bootstrap.js
+++ source/scripts/bootstrap.js
@@ -1,6 +1,10 @@
 import { $setVar } from 'wee-store';
 import { $setRef } from 'core/dom';
 import 'es6-promise/auto';
+import '../styles/global.scss';
+
+// Import all component scss files
+require.context('../components', true, /\.scss$/);
 
 $setRef();
 $setVar();
