440ad1a4de445db6a3efbd37c5fe82f3b4aa7f86
Peter Bedor <peter@lewiscommunications.com>
2019-05-29 15:26:28 -0500

    Fix indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,42 +2,42 @@ module.exports = {
     parserOptions: {
         parser: 'babel-eslint',
     },
-	extends: [
+    extends: [
         // https://github.com/airbnb/javascript
         'airbnb-base',
         // https://eslint.vuejs.org/rules/
         'plugin:vue/recommended',
     ],
-	plugins: [
+    plugins: [
         'import',
         'vue',
-	],
-	settings: {
-		'import/resolver': {
-			webpack: {
+    ],
+    settings: {
+        'import/resolver': {
+            webpack: {
                 config: './build/webpack.dev.config.js',
             },
-		},
+        },
     },
     rules: {
         strict: 0,
-		indent: ['error', 4],
-		'eol-last': 0,
-		'no-plusplus': 0,
-		'prefer-template': 0,
-		'import/prefer-default-export': 0,
-		'space-unary-ops': 0,
-		'newline-per-chained-call': 0,
-		radix: 0,
-		'import/no-dynamic-require': 0,
-		'no-param-reassign': [
-			'error',
-			{
-				ignorePropertyModificationsFor: [
-					'state'
-				],
-			},
+        indent: ['error', 4],
+        'eol-last': 0,
+        'no-plusplus': 0,
+        'prefer-template': 0,
+        'import/prefer-default-export': 0,
+        'space-unary-ops': 0,
+        'newline-per-chained-call': 0,
+        radix: 0,
+        'import/no-dynamic-require': 0,
+        'no-param-reassign': [
+            'error',
+            {
+                ignorePropertyModificationsFor: [
+                    'state'
+                ],
+            },
         ],
-        'vue/html-indent': ['error', 4]
-	},
+        'vue/html-indent': ['error', 4],
+    },
 };
