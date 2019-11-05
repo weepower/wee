c52b615604c795b707f524a1742bcf8ce2c37259
Peter Bedor <peter@lewiscommunications.com>
2019-11-04 21:48:30 -0600

    Test commit

--- source/components/welcome/index.js
+++ source/components/welcome/index.js
@@ -2,6 +2,14 @@ import { RouteHandler } from 'wee-routes';
 
 export default new RouteHandler({
     init() {
+<<<<<<< Updated upstream
         //
+=======
+        console.log('welcome');
+
+        const test = {
+            what: 'cool',
+        };
+>>>>>>> Stashed changes
     },
 });
